a05d2548e2a006e2b7fdf3dc800924a5fcc3b472
Thomas Gummerer <t.gummerer@gmail.com>
2017-11-22 22:30:19 +0000

    worktree: make add <path> <branch> dwim
    Currently 'git worktree add <path> <branch>', errors out when 'branch'
    is not a local branch.   It has no additional dwim'ing features that one
    might expect.
    
    Make it behave more like 'git checkout <branch>' when the branch doesn't
    exist locally, but a remote tracking branch uniquely matches the desired
    branch name, i.e. create a new branch from the remote tracking branch
    and set the upstream to the remote tracking branch.
    
    As 'git worktree add' currently just dies in this situation, there are
    no backwards compatibility worries when introducing this feature.
    
    Signed-off-by: Thomas Gummerer <t.gummerer@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t2025-worktree-add.sh
+++ t/t2025-worktree-add.sh
@@ -369,4 +369,25 @@ test_expect_success '--no-track avoids setting up tracking' '
 	)
 '
 
+test_expect_success '"add" <path> <non-existent-branch> fails' '
+	test_must_fail git worktree add foo non-existent
+'
+
+test_expect_success '"add" <path> <branch> dwims' '
+	test_when_finished rm -rf repo_upstream repo_dwim foo &&
+	setup_remote_repo repo_upstream repo_dwim &&
+	git init repo_dwim &&
+	(
+		cd repo_dwim &&
+		git worktree add ../foo foo
+	) &&
+	(
+		cd foo &&
+		test_branch_upstream foo repo_upstream foo &&
+		git rev-parse repo_upstream/foo >expect &&
+		git rev-parse foo >actual &&
+		test_cmp expect actual
+	)
+'
+
 test_done
